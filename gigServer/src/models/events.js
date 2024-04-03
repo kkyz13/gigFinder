f1ecbaf4f429c73ab1a720dc2587ce7490c42191
kevintch <KevinTanChayHan@hotmail.com>
2024-04-03 13:11:48 +0800

    Update events.js

--- gigServer/src/models/events.js
+++ gigServer/src/models/events.js
@@ -1,19 +1,26 @@
 const mongoose = require("mongoose");
+// To be added
+// const userListSchema = new mongoose.Schema(
+//   {
+//     id: { type: mongoose.ObjectId, ref: "UserAuth" },
+//   },
+//   { collection: "userList" }
+// );
 
 const EventsSchema = new mongoose.Schema(
   {
     //-------------Required-----------------//
     title: { type: String, require: true, minLength: 1, maxLength: 50 },
     author: { type: String, require: true, minLength: 1, maxLength: 50 },
-    address: { type: String, require: true, minLength: 1, maxLength: 50 },
     timeStart: { type: Date, require: true },
-    link: { type: String, require: true },
     //----------------(optional)---------------//
-    description: { type: String },
-    // interestUserList: [userSchema],
-    // subscribeUserList: [userSchema],
+    address: { type: String, minLength: 1, maxLength: 100 },
+    link: { type: String },
+    description: { type: String, maxLength: 1000 },
+    // interestUserList: [userListSchema],
+    // subscribeUserList: [userListSchema],
   },
-  { collection: "event" }
+  { collection: "events" }
 );
 
 module.exports = mongoose.model("Events", EventsSchema);
