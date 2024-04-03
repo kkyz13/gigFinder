f954e1e263ddc2685a54e82d0f49401b81977c40
Odelia Tan <odelia_tan@hotmail.com>
2024-04-03 21:32:33 +0800

    Create get all gigs endpoint

--- gigServer/src/routers/gigs.js
+++ gigServer/src/routers/gigs.js
@@ -1,9 +1,10 @@
 const express = require("express");
-const { addGigForProvider } = require("../controllers/gigs");
+const { addGigForProvider, getAllGigs } = require("../controllers/gigs");
 const { validateAddGigData } = require("../validators/gigs");
 const { errorCheck } = require("../validators/errorCheck");
 const router = express.Router();
 
+router.get("/gigs", getAllGigs);
 router.put(
   "/gigs/p/:providerId",
   validateAddGigData,
