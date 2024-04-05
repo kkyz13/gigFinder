fa36ac190bcefb517602195a685c65c5ae35a361
Kenneth Koh <kkyz13@gmail.com>
2024-04-05 10:16:35 +0800

    Update GigDetails.jsx

--- gigFinder_Front/src/components/GigDetails.jsx
+++ gigFinder_Front/src/components/GigDetails.jsx
@@ -1,8 +1,15 @@
 import React, { useContext } from "react";
 import UserContext from "../context/user";
+import useFetch from "../hooks/useFetch";
 
 const GigDisplay = (props) => {
+  const fetchData = useFetch();
   const userCtx = useContext(UserContext);
+  const loadGigDetails = () => {
+    if (props.entryId) {
+      // const res = await FetchData()
+    }
+  };
   return (
     <div className="col-6 gigdisplay">
       <div>accesstoken: {userCtx.accessToken}</div>
