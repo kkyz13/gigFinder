270b33039a77f854b1c983a3d22dc2ff52b88901
Kenneth Koh <kkyz13@gmail.com>
2024-04-08 10:16:59 +0800

    hotfix to gig validators
    allow address and link in

--- gigServer/src/validators/gigs.js
+++ gigServer/src/validators/gigs.js
@@ -3,11 +3,11 @@ const { body, param } = require("express-validator");
 const validateAddGigData = [
   body("title", "title is required").not().isEmpty(),
   body("dateTimeStart", "start date and time is required").not().isEmpty(),
-  body("address", "address is required").optional().not().isEmpty(),
-  body("address", "address must be between 1 and 100 characters")
-    .optional()
-    .isLength({ min: 1, max: 100 }),
-  body("link", "link is required").optional().not().isEmpty(),
+  // body("address", "address is required").optional().not().isEmpty(),
+  // body("address", "address must be between 1 and 100 characters")
+  //   .optional()
+  //   .isLength({ min: 1, max: 100 }),
+  // body("link", "link is required").optional().not().isEmpty(),
   body("description", "description must be less that 1000 characters")
     .optional()
     .isLength({ max: 1000 }),
@@ -19,11 +19,11 @@ const validateUpdateGigData = [
     .optional()
     .not()
     .isEmpty(),
-  body("address", "address is required").optional().not().isEmpty(),
-  body("address", "address must be between 1 and 100 characters")
-    .optional()
-    .isLength({ min: 1, max: 100 }),
-  body("link", "link is required").optional().not().isEmpty(),
+  // body("address", "address is required").optional().not().isEmpty(),
+  // body("address", "address must be between 1 and 100 characters")
+  //   .optional()
+  //   .isLength({ min: 1, max: 100 }),
+  // body("link", "link is required").optional().not().isEmpty(),
   body("description", "description must be less that 1000 characters")
     .optional()
     .isLength({ max: 1000 }),
